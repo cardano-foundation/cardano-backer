03626048626d7ea7c5662affcd47bb4bd329a581
Patrick Vu <patrick.vu@sotatek.com>
2024-10-16 17:45:04 +0700

    refactor: handle multiple events to submit on chain

--- src/backer/cardaning.py
+++ src/backer/cardaning.py
@@ -63,32 +63,32 @@ class Cardano:
         else:
             self.fundAddress(self.spending_addr)
 
-    def publishEvent(self, event: bytearray):
-        self.pending_kel = event
-        self.flushQueue()
+    def publishEvent(self, event: bytes):
+        self.pending_kel += event
 
     def flushQueue(self):
         try:
-            # Build transaction
-            builder = pycardano.TransactionBuilder(self.context)
-            builder.add_input_address(self.spending_addr)
-            builder.add_output(pycardano.TransactionOutput(self.spending_addr, pycardano.Value.from_primitive([TRANSACTION_AMOUNT])))
+            if self.pending_kel:
+                # Build transaction
+                builder = pycardano.TransactionBuilder(self.context)
+                builder.add_input_address(self.spending_addr)
+                builder.add_output(pycardano.TransactionOutput(self.spending_addr, pycardano.Value.from_primitive([TRANSACTION_AMOUNT])))
+                self.pending_kel = bytes(self.pending_kel)
+                # Chunk size
+                # bytearrays is not accept
+                value = [self.pending_kel[i:i + 64] for i in range(0, len(self.pending_kel), 64)]
 
-            # Chunk size
-            # bytearrays is not accept
-            value = [self.pending_kel[i:i + 64] for i in range(0, len(self.pending_kel), 64)]
+                # Metadata. accept int key type
+                builder.auxiliary_data = pycardano.AuxiliaryData(pycardano.Metadata({1: value}))
 
-            # Metadata. accept int key type
-            builder.auxiliary_data = pycardano.AuxiliaryData(pycardano.Metadata({1: value}))
-
-            signed_tx = builder.build_and_sign([self.payment_signing_key],
-                                               change_address=self.spending_addr,
-                                               merge_change=True)
-            # Submit transaction
-            self.context.submit_tx(signed_tx.to_cbor())            
-        except Exception as e:
-            self.timer = Timer(90, self.flushQueue)
-            self.timer.start()
+                signed_tx = builder.build_and_sign([self.payment_signing_key],
+                                                change_address=self.spending_addr,
+                                                merge_change=True)
+                # Submit transaction
+                self.context.submit_tx(signed_tx.to_cbor())  
+                self.pending_kel = bytearray()
+        except Exception as e:    
+            logger.critical(f"Error: {e}")
 
     def getaddressBalance(self, addr):
         try:            
